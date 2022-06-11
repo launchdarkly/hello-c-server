38dbad4725ad717e8324777cab5815ad822b883a
Casey Waldren <cwaldren@launchdarkly.com>
2022-06-10 17:01:40 -0700

    Use dylibs on MacOS

--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,1 @@
-clang -target x86_64-apple-darwin21.1.0 -o hello hello.c lib/libldserverapi.a -I include -lcurl -lpthread -lpcre -lm -framework CoreFoundation -framework IOKit
+clang -target x86_64-apple-darwin21.1.0 -o hello hello.c -L"./lib/" -I include -lcurl -lpthread -lpcre -lm -lldserverapi -framework CoreFoundation -framework IOKit
--- fetch-mac.sh
+++ fetch-mac.sh
@@ -3,6 +3,6 @@
 set -e
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/launchdarkly/c-server-sdk/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-ARTIFACT_URL="https://github.com/launchdarkly/c-server-sdk/releases/download/$LATEST_VERSION/osx-clang-64bit-static.zip"
-curl -o osx-clang-64bit-static.zip -L $ARTIFACT_URL
-unzip osx-clang-64bit-static.zip
+ARTIFACT_URL="https://github.com/launchdarkly/c-server-sdk/releases/download/$LATEST_VERSION/osx-clang-64bit-dynamic.zip"
+curl -o osx-clang-64bit-dynamic.zip -L $ARTIFACT_URL
+unzip osx-clang-64bit-dynamic.zip
